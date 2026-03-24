bb6339067818b52e0fbdc7f24d7ae62219cdb6b9
zeztto <zeztto@gmail.com>
2026-03-24 23:37:55 +0900

    feat: add www to non-www redirect in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server'
 import createMiddleware from 'next-intl/middleware'
 import { routing } from './i18n/routing'
 
-export default createMiddleware(routing)
+const intlMiddleware = createMiddleware(routing)
+
+export default function middleware(request: NextRequest) {
+  // Redirect www to non-www
+  const host = request.headers.get('host') || ''
+  if (host.startsWith('www.')) {
+    const url = request.nextUrl.clone()
+    url.host = host.replace('www.', '')
+    return NextResponse.redirect(url, 301)
+  }
+
+  return intlMiddleware(request)
+}
 
 export const config = {
   matcher: ['/', '/(ko|en)/:path*'],
